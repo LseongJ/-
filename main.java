4e2f6d145e9fdfbecb9fa6db9261783c4e9cb3d0
LseongJ <dltjdwo8697@naver.com>
2022-12-17 23:07:53 +0900

    123123123123

--- main.java
+++ main.java
@@ -1,77 +1,69 @@
+import java.util.*;
 import javax.swing.*;
+
 class Main {
     public static void main(String[] args) {
-        
-        while (true){
+        while (true) {
+            boolean flag = true;
             CardDeck carddeck = new CardDeck();   // 52장 생성
-            CardPlayer cp = new ComputerPlayer(5);
+            CardPlayer cp = new ComputerPlayer(52);
             CardPlayer hm = new HumanPlayer(52);
+            int money = 1000;
+            
+            System.out.println("얼마를 베팅할 건가요?");
+
+            //처음에 2장씩 가진다.
+            hm.receiveCard(carddeck.newCard());
+            hm.receiveCard(carddeck.newCard());
+            System.out.println(Arrays.toString(hm.showCards()));
 
             cp.receiveCard(carddeck.newCard());
             cp.receiveCard(carddeck.newCard());
 
-            hm.receiveCard(carddeck.newCard());
-            hm.receiveCard(carddeck.newCard());
-            System.out.println("플레이어의 처음 카드합: " + hm.get_sum());
-            System.out.println("딜러의 처음 카드합: " + cp.get_sum());
-
-            if (hm.get_sum() >= 22){       // hm이 22가 넘으면,
-                // 패배를 선언함
-                System.out.println("플레이어의 패배(Bust)");
-                // 패배
-                // 프로그램 종료
+            System.out.println("플레이어의 처음 카드합은 " + hm.get_sum() + " 입니다");
+            System.out.println("딜러의 처음 카드합은 " + cp.get_sum() + " 입니다");
+            
+            if (hm.get_sum() > 21) {
+                System.out.println("플레이어의 패배");
+                flag = false;
+            }
+            else if (hm.get_sum() == 21) {
+                System.out.println("플레이어의 승리(Blackjack!!!)");
+                flag = false;
+            }
+            else if (cp.get_sum() > 21) {
+                System.out.println("플레이어의 승리");
+                flag = false;
             }
 
-            else if (hm.get_sum() == 21){
-                System.out.println("플레이어의 승리(Blackjack1)");
-                // 승리
-                // 프로그램 종료
-            }
-            else if (cp.get_sum() == 21){
-                System.out.println("딜러의 승리(Blackjack1)");
-                // 승리
-                // 프로그램 종료
-            }
-
-            else if (hm.get_sum() < 21 && cp.get_sum() >= 22){
-                // 승리
-                // 종료
-                System.out.println("플레이어의 승리2)");
-            }
-
-            // 둘 다 초기sum < 21인 경우
             boolean cp_stay = false;
             boolean hm_stay = false;
-            while (true) {
-                if (hm_stay == false && (hm.wantsACard() == true)){   //hit
-                    hm.receiveCard(carddeck.newCard());
+            while (flag) {
+                if (hm_stay == false && (hm.wantsACard() == true)){
+                    hm.receiveCard(carddeck.newCard());     // hit
                     System.out.println("플레이어의 카드합: " + hm.get_sum());
-                    if (hm.get_sum() >= 22){       // hm이 22가 넘으면,
-                        // 패배를 선언함
-                        System.out.println("플레이어의 패배(Bust)");
-                        // 패배
-                        // 프로그램 종료
+                    if (hm.get_sum() > 21) {
+                        System.out.println("플레이어의 패배");
+                        flag = false;
                         break;
                     }
-            
-                    else if (hm.get_sum() == 21){
-                        System.out.println("플레이어의 승리3(Blackjack!!!)");
-                        // 승리
-                        // 프로그램 종료
+                    else if (hm.get_sum() == 21) {
+                        System.out.println("플레이어의 승리(Blackjack!!!)");
+                        flag = false;
                         break;
-                    }     
+                    }
                 }
                 else{
                     hm_stay = true;
                 }
-                if (cp_stay == false && (cp.wantsACard() == true)){   //hit
-                    cp.receiveCard(carddeck.newCard());
-                    System.out.println("딜러의 카드합: " + cp.get_sum());
-                    if (cp.get_sum() >= 22){
-                        System.out.println("플레이어의 승리4");
+                if (cp_stay == false && (cp.wantsACard() == true)){
+                    cp.receiveCard(carddeck.newCard());     // hit
+                    System.out.println("딜러의 카드합은: " + cp.get_sum());
+                    if (cp.get_sum() > 21) {
+                        System.out.println("플레이어의 승리");
+                        flag = false;
                         break;
-                    }    
-                    
+                    }
                 }
                 else{
                     cp_stay = true;
@@ -80,28 +72,26 @@ class Main {
                     break;
                 }
             }
-
-            if (cp.get_sum() > hm.get_sum()){
-                System.out.println("딜러의 승리");
+            if (flag) {
+                if (hm.get_sum() > cp.get_sum()) {
+                    System.out.println("플레이어의 승리");
+                }
+                else if (cp.get_sum() > hm.get_sum()) {
+                    System.out.println("플레이어의 패배");
+                }
+                else {
+                    System.out.println("무승부");
+                }
             }
-            else if (cp.get_sum() < hm.get_sum() && hm.get_sum() < 21){
-                System.out.println("플레이어의 승리5");
-            }
-            else if (cp.get_sum() == hm.get_sum() && hm.get_sum() < 21){
-                System.out.println("무승부"); }
-            
-
             String response = JOptionPane.showInputDialog
-            ("Do you want play more? (Y or N)?"); 
-            if (response.equals("Y") == true){
-                System.out.println("더한다는뎁쇼?");
-            }
-            else {
-                System.out.println("응안해");
-                break;
-            }
-
-
+                ("Do you want play more? (Y or N)?"); 
+                if (response.equals("Y") == true){
+                    System.out.println("더한다는뎁쇼?");
+                }
+                else {
+                    System.out.println("응안해");
+                    break;
+                }
         }
     }
 }
